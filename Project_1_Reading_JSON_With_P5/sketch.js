edd04f0216cc9a7ed0fb85b84cddec4164a7f29d
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2017-11-19 10:43:25 +0100

    Add Project 1

--- Project_1_Reading_JSON_With_P5/sketch.js
+++ Project_1_Reading_JSON_With_P5/sketch.js
@@ -1,11 +1,18 @@
-var flower = {
+var flower;
 
-};
+//loading JSON is preload because it loads it asynchronously and if we want
+//all data to be available we load it in preload because this is the first
+//function that is called.
+function preload(){
+  flower = loadJSON("flower.json");
+}
 
 function setup() {
-
+  createCanvas(400, 400);
 }
 
 function draw() {
-
+  background(0);
+  fill(flower.r, flower.g, flower.b);
+  text(flower.name, 10, 50);
 }
